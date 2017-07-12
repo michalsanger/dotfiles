609b564bdeba733be1da84edb0b52f0cf9c3965f
Mathias Bynens <mathias@qiwi.be>
2017-07-12 18:23:53 +0200

    brew.sh: Clean up

--- brew.sh
+++ brew.sh
@@ -38,11 +38,9 @@ brew install gnupg
 
 # Install more recent versions of some macOS tools.
 brew install vim --with-override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install homebrew/php/php70
-brew install homebrew/php/composer
+brew install grep
+brew install openssh
+brew install screen
 
 # Install font tools.
 brew tap bramstein/webfonttools
@@ -78,7 +76,6 @@ brew install xz
 
 # Install other useful binaries.
 brew install ack
-brew install dark-mode
 #brew install exiv2
 brew install git
 brew install git-lfs
@@ -89,13 +86,9 @@ brew install p7zip
 brew install pigz
 brew install pv
 brew install rename
-brew install rhino
-brew install speedtest_cli
 brew install ssh-copy-id
-brew install testssl
 brew install tree
 brew install vbindiff
-brew install webkit2png
 brew install zopfli
 brew install autopano-sift-c
 brew install s3cmd
