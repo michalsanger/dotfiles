e787beaf365a5a87c09f7efaf4dd4e2251b2252c
Michal Sanger <michal.sanger@gmail.com>
2019-06-27 15:53:35 +0200

    Brew install updated

--- brew.sh
+++ brew.sh
@@ -94,5 +94,122 @@ brew install tree
 brew install vbindiff
 brew install zopfli
 
+brew install adns
+brew install autoconf
+brew install autopano-sift-c
+brew install cocoapods
+brew install dark-mode
+brew install exif
+brew install exiftool
+brew install freetype
+brew install gcc
+brew install gdbm
+brew install gettext
+brew install gmp
+brew install gnutls
+brew install gpg-agent
+brew install grunt-cli
+brew install htop
+brew install httpstat
+brew install icu4c
+brew install ilmbase
+brew install isl
+brew install jasper
+brew install jpeg
+brew install jq
+brew install ldns
+brew install libassuan
+brew install libde265
+brew install libdnet
+brew install libevent
+brew install libexif
+brew install libffi
+brew install libgcrypt
+brew install libgpg-error
+brew install libheif
+brew install libidn2
+brew install libksba
+brew install libmpc
+brew install libomp
+brew install libpano
+brew install libpng
+brew install libtasn1
+brew install libtiff
+brew install libtool
+brew install libunistring
+brew install libusb
+brew install libxml2
+brew install libyaml
+brew install little-cms2
+brew install mpfr
+brew install narwhal
+brew install ncurses
+brew install nettle
+brew install node
+brew install npth
+brew install oniguruma
+brew install openexr
+brew install openjpeg
+brew install openssl
+brew install openssl@1.1
+brew install p11-kit
+brew install pcre
+brew install pcre2
+brew install pdfcrack
+brew install perl
+brew install pinentry
+brew install pinentry-mac
+brew install pipenv
+brew install pkg-config
+brew install popt
+brew install pth
+brew install pyenv
+brew install python
+brew install readline
+brew install rhino
+brew install ringojs
+brew install ruby
+brew install s3cmd
+brew install speedtest-cli
+brew install sqlite
+brew install ssdeep
+brew install terraform
+brew install testssl
+brew install unbound
+brew install unixodbc
+brew install unrar
+brew install vault
+brew install watchman
+brew install webkit2png
+brew install webp
+brew install x265
+brew install yarn
+brew install youtube-dl
+
 # Remove outdated versions from the cellar.
 brew cleanup
+
+brew cask install adobe-acrobat-reader
+brew cask install atom
+brew cask install charles
+brew cask install disk-inventory-x
+brew cask install docker
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install grammarly
+brew cask install graphiql
+brew cask install java
+brew cask install keybase
+brew cask install libreoffice
+brew cask install meld
+brew cask install ngrok
+brew cask install opera
+brew cask install postman
+brew cask install skype
+brew cask install slack
+brew cask install sourcetree
+brew cask install sqlectron
+brew cask install sublime-text
+brew cask install tower
+brew cask install vlc
+brew cask install zeplin
